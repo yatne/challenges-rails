2962e317ba33e9ce9da82e39de8c50c5ed3eb12f
yatne <antonimaciej@gmail.com>
2024-02-12 15:08:46 +0100

    font change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['"Roboto"', ...defaultTheme.fontFamily.sans],
+      },
+    }
+  }
 }
